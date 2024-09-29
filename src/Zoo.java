db4bc5a50c8fd342b9b965df827fb8addf741e95
medaminenj <147668061+medaminenj@users.noreply.github.com>
2024-09-29 08:39:58 +0200

    Zoo.java

--- src/Zoo.java
+++ src/Zoo.java
@@ -1,21 +1,33 @@
-public class Zoo {
-    private Animal[] animals;
-    private String name;
-    private String city;
-    private int nbrCages;
 
-    public Zoo(Animal[] animals, String name, String city, int nbrCages) {
-        this.animals = animals;
+import java.util.Arrays;
+
+public class Zoo {
+
+    String name;
+    String city;
+    int nbrcages;
+    Animal[] animals;
+
+    public Zoo(String name, String city, int nbrcages, Animal[] animals) {
         this.name = name;
         this.city = city;
-        this.nbrCages = nbrCages;
+        this.nbrcages = nbrcages;
+        this.animals = new Animal[nbrcages];
     }
 
-    public void displayZoo() {
-        System.out.println("Nom du zoo : " + name);
-        System.out.println("Ville : " + city);
-        System.out.println("Nombre de cages : " + nbrCages);
+    public void displayZoo(){
+        System.out.println("zoo name: " + name);
+        System.out.println("zoo city: " + city);
+        System.out.println("zoo nbrcages: " + nbrcages);
     }
 
-    
+    @Override
+    public String toString() {
+        return "Zoo{" +
+                "name='" + name + '\'' +
+                ", city='" + city + '\'' +
+                ", nbrcages=" + nbrcages +
+                ", animals=" + Arrays.toString(animals) +
+                '}';
+    }
 }
