00f58f24fde97386b2f6f8af9d8a2d9eb14fd80c
medaminenj <147668061+medaminenj@users.noreply.github.com>
2024-09-29 08:28:48 +0200

    Animal.java

--- src/Animal.java
+++ src/Animal.java
@@ -15,35 +15,5 @@ public class Animal {
         System.out.println(name + ", " + age + " ans, Famille : " + family + ", Mammifère : " + (isMammal ? "Oui" : "Non"));
     }
 
-    public String getFamily() {
-        return family;
-    }
 
-    public void setFamily(String family) {
-        this.family = family;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public boolean isMammal() {
-        return isMammal;
-    }
-
-    public void setMammal(boolean isMammal) {
-        this.isMammal = isMammal;
-    }
 }
